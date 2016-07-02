c7e18df6b18d0365e06329b8cad84218121c95ed
ekongx <ing@live.cn>
2016-07-02 15:28:05 +0800

    Create addInfo.py

---
+++ addInfo.py
@@ -0,0 +1,13 @@
+import datetime
+import sublime_plugin
+class AddInfoCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet",
+            {
+                "contents": "/**""\n"
+                " * ""\n"
+                " * Author:    LYL""\n"
+                " * DateTime:  "  "%s"  %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +"\n"                
+                " */"
+            }
+        )
